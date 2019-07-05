a4dc7bea60306cd99bb444bdf9b3fdc55866e555
wxj <825754896@qq.com>
2019-07-06 02:47:59 +0800

    ui拖拽

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class Block : MonoBehaviour,IDragHandler,IEndDragHandler{
+public class Block : MonoBehaviour,IDragHandler,IEndDragHandler,IBeginDragHandler{
 
+    [SerializeField]
+    private BlockManager bm;
     private Vector3 mousePos;
     private Vector3 initialSelfPos;
     private RectTransform rt;
+    private bool inBubble;
+    /*[SerializeField]
+    private float offset;*/
+    [SerializeField]
+    private float fitrange;
+    private GameObject[] bubbles;
+    private Vector3 prePos;
 
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
@@ -20,8 +29,13 @@ public class Block : MonoBehaviour,IDragHandler,IEndDragHandler{
 
     void IEndDragHandler.OnEndDrag(PointerEventData eventData)
     {
-        rt.position = initialSelfPos;
-        Debug.Log("drag ended");
+        if (IntoBubble())
+            inBubble = true;
+        else if (IntoBlock())
+            inBubble = false;
+        else
+            this.transform.position = prePos;
+        //Debug.Log("drag ended"+"  "+rt.position);
     }
 
 
@@ -29,6 +43,7 @@ public class Block : MonoBehaviour,IDragHandler,IEndDragHandler{
     void Start () {
         rt = this.GetComponent<RectTransform>();
         initialSelfPos = rt.position;
+        bubbles = GameObject.FindGameObjectsWithTag("Bubble");
 	}
 	
 	// Update is called once per frame
@@ -36,5 +51,47 @@ public class Block : MonoBehaviour,IDragHandler,IEndDragHandler{
 		
 	}
 
+    bool IntoBubble()
+    {
+        if(!inBubble)
+        {
+            for(int i=0;i<bubbles.Length;i++)
+            {
+                
+                if(Mathf.Abs((this.transform.position-bubbles[i].transform.position).magnitude) <= fitrange)
+                {
+                    Debug.Log((this.transform.position - bubbles[i].transform.position).magnitude);
+                    this.transform.position = bubbles[i].transform.position;
+                    Debug.Log("fit in");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
+    bool IntoBlock()
+    {
+        if(!inBubble)
+        {
+            
+            foreach(Vector3 block in bm.blocksPos)
+            {
+                if(Mathf.Abs((this.transform.position - block).magnitude)<=fitrange)
+                {
+                    Debug.Log((this.transform.position - block).magnitude);
+                    this.transform.position = block;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("begin drag   " + this.transform.position);
+        inBubble=false;
+        prePos = this.transform.position;
+    }
 }
---
+++ Assets/Scripts/BlockManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour {
+
+    public  List<Vector3> blocksPos;
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] blocks = GameObject.FindGameObjectsWithTag("Block");
+        foreach (GameObject block in blocks)
+        {
+            Debug.Log(block.transform.position);
+            blocksPos.Add(block.transform.position);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
