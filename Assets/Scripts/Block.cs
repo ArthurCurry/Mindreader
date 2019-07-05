d0fae5cf9f20ffa05b8692db7b25a426bf0c61d6
wxj <825754896@qq.com>
2019-07-06 06:53:49 +0800

    拖拽优化

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,6 +8,10 @@ public class Block : MonoBehaviour,IDragHandler,IEndDragHandler,IBeginDragHandle
 
     [SerializeField]
     private BlockManager bm;
+    [SerializeField]
+    private Canvas canvas;
+    [SerializeField]
+    private GameObject anchor;
     private Vector3 mousePos;
     private Vector3 initialSelfPos;
     private RectTransform rt;
@@ -65,10 +69,11 @@ public class Block : MonoBehaviour,IDragHandler,IEndDragHandler,IBeginDragHandle
             for(int i=0;i<bubbles.Length;i++)
             {
                 
-                if(Mathf.Abs((this.transform.position-bubbles[i].transform.position).magnitude) <= fitrange)
+                if(Mathf.Abs((this.transform.position-bubbles[i].transform.position).magnitude) <= fitrange&&!Detect(bubbles[i]))
                 {
                     Debug.Log((this.transform.position - bubbles[i].transform.position).magnitude);
                     this.transform.position = bubbles[i].transform.position;
+                    transform.SetParent(canvas.transform);
                     Debug.Log("fit in");
                     return true;
                 }
@@ -82,12 +87,14 @@ public class Block : MonoBehaviour,IDragHandler,IEndDragHandler,IBeginDragHandle
         if(!inBubble)
         {
             
-            foreach(Vector3 block in bm.blocksPos)
+            foreach(GameObject box in bm.boxes)
             {
-                if(Mathf.Abs((this.transform.position-cameraOffset - block).magnitude)<=fitrange)
+                Debug.Log((this.transform.position - box.transform.position).magnitude);
+                if ((this.transform.position- box.transform.position).magnitude<=fitrange&&!Detect(box))
                 {
-                    Debug.Log((this.transform.position - cameraOffset - block).magnitude);
-                    this.transform.position = block+cameraOffset;
+                    
+                    this.transform.position = box.transform.position;
+                    transform.SetParent(anchor.transform);
                     return true;
                 }
             }
@@ -97,7 +104,7 @@ public class Block : MonoBehaviour,IDragHandler,IEndDragHandler,IBeginDragHandle
 
     void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("begin drag   " + this.transform.position);
+        //Debug.Log("begin drag   " + this.transform.position);
         inBubble=false;
         posB4Drag = this.transform.position;
     }
@@ -110,5 +117,15 @@ public class Block : MonoBehaviour,IDragHandler,IEndDragHandler,IBeginDragHandle
         }
     }
 
-
+    bool Detect(GameObject target)
+    {
+        foreach(GameObject block in bm.blocks)
+        {
+            if(block.transform!=this.transform&&(block.transform.position-target.transform.position).magnitude<fitrange)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class BlockManager : MonoBehaviour {
 
     public  List<Vector3> blocksPos;
-
-	// Use this for initialization
-	void Start () {
-        GameObject[] blocks = GameObject.FindGameObjectsWithTag("Block");
+    public GameObject[] blocks;
+    public GameObject[] boxes;
+    // Use this for initialization
+    void Start () {
+        blocks = GameObject.FindGameObjectsWithTag("Block");
         foreach (GameObject block in blocks)
         {
             Debug.Log(block.transform.position);
             blocksPos.Add(block.transform.position);
         }
-        
+        boxes = GameObject.FindGameObjectsWithTag("Box");
 	}
 	
 	// Update is called once per frame
