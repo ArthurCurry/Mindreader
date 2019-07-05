2ba42f2ded6b6098d86607a9f49de195ab4d8a27
wxj <825754896@qq.com>
2019-07-05 21:14:13 +0800

    ui拖拽

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Block : MonoBehaviour,IDragHandler,IEndDragHandler{
+
+    private Vector3 mousePos;
+    private Vector3 initialSelfPos;
+    private RectTransform rt;
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        Vector3 pos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rt, eventData.position, eventData.enterEventCamera, out pos);
+        rt.position = pos;
+        
+    }
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        rt.position = initialSelfPos;
+        Debug.Log("drag ended");
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        rt = this.GetComponent<RectTransform>();
+        initialSelfPos = rt.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
