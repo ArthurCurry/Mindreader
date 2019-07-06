6ed2288f7c16dffea99fefc20cfe37d75dd2798c
wxj <825754896@qq.com>
2019-07-06 10:22:03 +0800

    xml对话系统1

---
+++ Assets/Scripts/Talk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.IO;
+
+public class Talk : MonoBehaviour {
+
+    [SerializeField]
+    private string characterName;
+    private int dialogCount;
+    private int index;
+    private List<string> dialogs;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
