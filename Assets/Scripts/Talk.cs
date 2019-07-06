6401e6db88153be9cd2e9673cf14c728f0d16b52
wxj <825754896@qq.com>
2019-07-06 15:02:40 +0800

    xml对话3

--- Assets/Scripts/Talk.cs
+++ Assets/Scripts/Talk.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Xml;
 using System.IO;
+using UnityEngine.EventSystems;
 
-public class Talk : MonoBehaviour {
+public class Talk : MonoBehaviour,IPointerClickHandler {
 
     [SerializeField]
     private string characterName;
+    [SerializeField]
+    private GameObject bubble;
+    [SerializeField]
+    private TextMesh text;
     private int dialogCount;
     private int index;
-    private List<string> dialogs;
+    private List<string> dialogs=new List<string>();
+    private List<string> titles=new List<string>();
+    private List<int> paraLength;
+    [SerializeField]
+    private GameObject playerText;
+    XmlDocument xml = new XmlDocument();
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        string data = Resources.Load("Dialogues/dialogues").ToString();
+        xml.LoadXml(data);
+        LoadDialog(characterName);
 	}
 	
 	// Update is called once per frame
@@ -24,6 +36,28 @@ public class Talk : MonoBehaviour {
 
     void LoadDialog(string name)
     {
+        XmlNode self = xml.SelectSingleNode("//"+name);
+        XmlNodeList list1 = self.SelectNodes("//对话");
+        foreach(XmlNode node in list1)
+        {
+            dialogs.Add(node.InnerText);
+            //Debug.Log(dialogs[dialogs.Count-1]);
+        }
+        XmlNodeList list2 = self.SelectNodes("//标题");
+        foreach(XmlNode node in list2)
+        {
+            titles.Add(node.InnerText);
+            Debug.Log(titles[titles.Count - 1]);
+        }
+    }
 
+    void UpdateDialog()
+    {
+
+    }
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        UpdateDialog();
     }
 }
