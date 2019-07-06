94604f63f6a4e7c31e8f848ec5b91c9275eb84ea
wxj <825754896@qq.com>
2019-07-07 01:40:52 +0800

    按钮

--- Assets/Scripts/RankMove.cs
+++ Assets/Scripts/RankMove.cs
@@ -4,28 +4,17 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class RankMove : MonoBehaviour,IPointerEnterHandler {
+public class RankMove : MonoBehaviour,IPointerEnterHandler{
 
     private GameObject character;
     public List<string> names = new List<string>();
     private int namesIndex;
     private Text text;
+    private Rect ButtonRect;
 
     void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
     {
-        if(Input.GetAxis("Mouse ScrollWheel")>0)
-        {
-            namesIndex += 1;
-            if (namesIndex >= names.Count)
-                namesIndex = 0;
-        }
-        else if(Input.GetAxis("Mouse ScrollWheel") < 0)
-        {
-            namesIndex -= 1;
-            if (namesIndex <= 0)
-                namesIndex = names.Count - 1;
-        }
-        text.text = names[namesIndex];
+        
     }
 
     // Use this for initialization
@@ -36,6 +25,25 @@ public class RankMove : MonoBehaviour,IPointerEnterHandler {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            Debug.Log(names.Count);
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                namesIndex += 1;
+
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                namesIndex -= 1;
+            }
+            if (namesIndex < 0)
+                namesIndex = names.Count - 1;
+            if (namesIndex >= names.Count)
+                namesIndex = 0;
+            if (this.gameObject.activeInHierarchy)
+                text.text = names[namesIndex];
+        }
+        
+    }
 }
